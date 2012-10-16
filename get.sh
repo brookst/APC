c73a2b364f7a3ae144cfeee75fc7a1b0b4a4ebc6
Tim Brooks <brooks@cern.ch>
2012-10-16 20:50:14 +0100

    Tidied up struture, fixing submodules

--- get.sh
+++
@@ -1,6 +0,0 @@
-git submodule init https://github.com/skluth/INIParser.git
-git submodule init https://github.com/skluth/RooAverageTools.git
-git submodule init https://github.com/skluth/RooConstrainedFit.git
-git submodule init https://github.com/skluth/AverageTools.git
-git submodule init https://github.com/skluth/ConstrainedFit.git
-git submodule init https://github.com/skluth/RooUnfold.git
---
+++ get_submodules.sh
@@ -0,0 +1,7 @@
+git submodule init INIParser
+git submodule init RooAverageTools
+git submodule init RooConstrainedFit
+git submodule init AverageTools
+git submodule init ConstrainedFit
+git submodule init RooUnfold
+git submodule update
